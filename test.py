0c7fb65a3b823214daff930668ae7eb779ab2234
rakshak-raina <45091369+rakshak-raina@users.noreply.github.com>
2019-10-21 14:05:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 from keras.models import load_model
 from load_data import data
 
+#input path
 path = input("Path: ")
 
+#model loading
 model = load_model("models/"+path+"/model.h5")
 
+#split data load
 left,right,out = data(50)
 
+# model prediction
 run = model.predict([left,right])
 
 print(run)
